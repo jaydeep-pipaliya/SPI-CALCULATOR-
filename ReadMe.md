bb00356a5f27be3ce11dfaf29b81efb8a526967d
zeel-codder <zeelprajapati321@gmail.com>
2021-03-11 10:47:22 +0530

    adding niram sem and branch api as niram_data.py

---
+++ ReadMe.md
@@ -0,0 +1,17 @@
+# Niram Python Praject Sem-4
+
+***Roll no***
+
+19BCE212-ZEEL PRAJAPATI
+
+19BCE207-AMIR KHAN
+
+19BCE208-JAYDEEP PATOLIYA
+
+***PROJECT***
+
+SPI CALCULATOR
+
+***Discription***
+
+This project is used to find spi of student of niram univercity.
--- tem.py
+++ tem.py
@@ -1,1 +1,36 @@
+# ===================================
+# This file is used to crate Api of niram per sem semester wise data
+# ===================================
+
+f1=open("Nirma_Data.txt","r")
+data=f1.read()
+list1=data.split("End")
+persem_list=list(map(lambda x: x.split("\n"),list1))
+Data_inDic={}
+tem=1
+last=-1
+for i in persem_list:
+    i.remove("")
+    if "" in i:
+        i.remove("")
+    if(i[0][0:8]!="Semester"):
+        if tem !=1:
+            Data_inDic[last]=tem
+        last=i[0]
+        i.remove(i[0])
+        tem={}
+    tem[i[0]]=i[1:]
+
+for i in Data_inDic:
+    # print(i):
+    for sem in Data_inDic[i]:
+        # print(f'{sem} {i}')
+        list=Data_inDic[i][sem]
+        for i1 in range(len(list)):
+            list[i1]=list[i1].split("-")
 import json
+f1.close()
+f1=open("Niram_data.py","w")
+f1.write("DATA=")
+f1.write(json.dumps(Data_inDic))
+f1.close()
